8bb28a1fb5d0affcabb8914103618219454520c3
Boban Acimovic <boban.acimovic@gmail.com>
2021-02-25 00:17:19 +0100

    implement lettre client

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,84 +1,26 @@
-use async_smtp::smtp::{authentication, response};
-use async_smtp::{EmailAddress, Envelope, SendableEmail, SmtpClient};
-use gethostname::gethostname;
-use rand::Rng;
-use serde::Deserialize;
-use std::process;
-use std::time::SystemTime;
-use std::u32;
+use lettre::{
+    transport::smtp::{authentication::Credentials, response::Response, Error},
+    AsyncSmtpTransport, Message, Tokio1Connector, Tokio1Transport,
+};
+use std::result::Result;
 
-#[derive(Debug, Deserialize)]
-pub struct Message {
-    pub from: String,
-    pub to: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub cc: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub bcc: Option<String>,
-    pub subject: String,
-    pub body: String,
-}
-
-#[derive(Clone)]
 pub struct Mailer {
-    creds: Option<authentication::Credentials>,
+    transport: AsyncSmtpTransport<Tokio1Connector>,
 }
 
 impl Mailer {
-    #[allow(dead_code)]
-    pub fn new() -> Self {
-        Self { creds: None }
+    pub fn new(smtp_server: String, smtp_auth_user: String, smtp_auth_passwd: String) -> Self {
+        let creds = Credentials::new(smtp_auth_user, smtp_auth_passwd);
+
+        let mailer = AsyncSmtpTransport::<Tokio1Connector>::relay(&smtp_server)
+            .unwrap()
+            .credentials(creds)
+            .build();
+
+        Self { transport: mailer }
     }
 
-    pub fn with_creds(smtp_auth_user: String, smtp_auth_passwd: String) -> Self {
-        Self {
-            creds: Some(authentication::Credentials::new(
-                smtp_auth_user,
-                smtp_auth_passwd,
-            )),
-        }
-    }
-
-    pub async fn send_mail(&self, m: &Message) -> Result<response::Response, anyhow::Error> {
-        let message = format!(
-            "From: {}\nTo: {}\nMessage-ID: {}\nSubject: {}\n\n{}",
-            &m.from,
-            &m.to,
-            self.message_id()?,
-            m.subject,
-            m.body
-        );
-
-        let envelope = Envelope::new(
-            Some(EmailAddress::new(m.from.clone())?),
-            vec![EmailAddress::new(m.to.clone())?],
-        )?;
-
-        let email = SendableEmail::new(envelope, "id".to_string(), message.into_bytes());
-
-        let mut smtp = SmtpClient::new("smtp.ectobit.com").await?;
-
-        if let Some(creds) = &self.creds {
-            smtp = smtp.credentials(creds.clone());
-        }
-
-        let result = smtp.into_transport().connect_and_send(email).await?;
-
-        Ok(result)
-    }
-
-    fn message_id(&self) -> anyhow::Result<String> {
-        let t = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
-
-        let mut rng = rand::thread_rng();
-        let r: u32 = rng.gen();
-
-        Ok(format!(
-            "<{}.{}.{}@{}>",
-            t.as_nanos(),
-            process::id(),
-            r,
-            gethostname().into_string().unwrap()
-        ))
+    pub async fn send(&self, message: Message) -> Result<Response, Error> {
+        self.transport.send(message).await
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
-use serde_json::value;
+// use serde_json::value;
 use std::env;
-use tide::prelude::*;
 
 mod mailer;
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    tide::log::start();
-
-    let smtp_client = mailer::Mailer::with_creds(
+#[tokio::main]
+async fn main() {
+    let _smtp_client = mailer::Mailer::new(
+        env::var("SMTP_HOST").unwrap(),
         env::var("SMTP_AUTH_USER").unwrap(),
         env::var("SMTP_AUTH_PASSWD").unwrap(),
     );
-
-    let mut app = tide::with_state(State {
-        mailer: smtp_client,
-    });
-    app.at("/mail").post(send_mail);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
 }
 
-#[derive(Clone)]
-pub struct State {
-    mailer: mailer::Mailer,
-}
+// #[derive(Clone)]
+// pub struct State {
+//     mailer: mailer::Mailer,
+// }
 
-async fn send_mail(mut req: tide::Request<State>) -> tide::Result<value::Value> {
-    let m: mailer::Message = req.body_json().await?;
-    req.state().mailer.send_mail(&m).await?;
-    Ok(json!({
-        "code": 200,
-        "success": true
-    }))
-}
+// async fn send_mail(mut req: tide::Request<State>) -> tide::Result<value::Value> {
+//     let m: mailer::Message = req.body_json().await?;
+//     // req.state().mailer.send_mail(&m).await?;
+//     Ok(json!({
+//         "code": 200,
+//         "success": true
+//     }))
+// }
