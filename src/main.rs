8f481da107c12b53c695b0a7eab68cc5c414b186
Boban Acimovic <boban.acimovic@gmail.com>
2021-02-25 00:29:11 +0100

    add mail example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 // use serde_json::value;
 use std::env;
+use lettre::Message;
 
 mod mailer;
 
 #[tokio::main]
 async fn main() {
-    let _smtp_client = mailer::Mailer::new(
+    let mailer = mailer::Mailer::new(
         env::var("SMTP_HOST").unwrap(),
         env::var("SMTP_AUTH_USER").unwrap(),
         env::var("SMTP_AUTH_PASSWD").unwrap(),
     );
+
+    let email = Message::builder()
+        .from("NoBody <nobody@domain.tld>".parse().unwrap())
+        .reply_to("John Doe <jdoe@domain.tld>".parse().unwrap())
+        .to("Hei <hei@domain.tld>".parse().unwrap())
+        .subject("Happy new async year")
+        .body(String::from("Be happy with async!"))
+        .unwrap();
+
+    mailer.send(email).await.unwrap();
 }
 
 // #[derive(Clone)]
